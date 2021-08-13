347ec8367a89efc9032609d93d0b12e1e285a243
Elias Garcia <eliasgarciadev@gmail.com>
2021-08-13 17:30:40 +0200

    Fix font paths

--- src/views/app.styles.js
+++ src/views/app.styles.js
@@ -5,42 +5,32 @@ const useAppStyles = createUseStyles((theme) => ({
   '@font-face': [
     {
       fontFamily: 'Modern Era',
-      src: "url('/fonts/modern-era/ModernEra-Regular.woff2') format('woff2')",
+      src: "url('fonts/modern-era/ModernEra-Regular.woff2') format('woff2')",
       fallbacks: [
-        { src: "url('/fonts/modern-era/ModernEra-Regular.woff') format('woff')" },
-        { src: "url('/fonts/modern-era/ModernEra-Regular.ttf') format('truetype')" }
+        { src: "url('fonts/modern-era/ModernEra-Regular.woff') format('woff')" },
+        { src: "url('fonts/modern-era/ModernEra-Regular.ttf') format('truetype')" }
       ],
       fontWeight: 400,
       fontStyle: 'normal'
     },
     {
       fontFamily: 'Modern Era',
-      src: "url('/fonts/modern-era/ModernEra-Medium.woff2') format('woff2')",
+      src: "url('fonts/modern-era/ModernEra-Medium.woff2') format('woff2')",
       fallbacks: [
-        { src: "url('/fonts/modern-era/ModernEra-Medium.woff') format('woff')" },
-        { src: "url('/fonts/modern-era/ModernEra-Medium.ttf') format('truetype')" }
+        { src: "url('fonts/modern-era/ModernEra-Medium.woff') format('woff')" },
+        { src: "url('fonts/modern-era/ModernEra-Medium.ttf') format('truetype')" }
       ],
       fontWeight: 500,
       fontStyle: 'normal'
     },
     {
       fontFamily: 'Modern Era',
-      src: "url('/fonts/modern-era/ModernEra-Bold.woff2') format('woff2')",
+      src: "url('fonts/modern-era/ModernEra-Bold.woff2') format('woff2')",
       fallbacks: [
-        { src: "url('/fonts/modern-era/ModernEra-Bold.woff') format('woff')" },
-        { src: "url('/fonts/modern-era/ModernEra-Bold.ttf') format('truetype')" }
+        { src: "url('fonts/modern-era/ModernEra-Bold.woff') format('woff')" },
+        { src: "url('fonts/modern-era/ModernEra-Bold.ttf') format('truetype')" }
       ],
       fontWeight: 700,
-      fontStyle: 'normal'
-    },
-    {
-      fontFamily: 'Modern Era',
-      src: "url('/fonts/modern-era/ModernEra-ExtraBold.woff2') format('woff2')",
-      fallbacks: [
-        { src: "url('/fonts/modern-era/ModernEra-ExtraBold.woff') format('woff')" },
-        { src: "url('/fonts/modern-era/ModernEra-ExtraBold.ttf') format('truetype')" }
-      ],
-      fontWeight: 800,
       fontStyle: 'normal'
     }
   ],
