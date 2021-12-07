5c481fc490dc8831881a64747ad2089967103839
Max Alekseenko <nnwen0609@gmail.com>
2021-12-07 16:18:08 +0300

    add note

--- src/views/tx-overview/tx-overview.view.jsx
+++ src/views/tx-overview/tx-overview.view.jsx
@@ -3,6 +3,7 @@ import useTxOverviewStyles from './tx-overview.styles'
 import { ReactComponent as CheckIcon } from '../../images/check-icon.svg'
 import { ReactComponent as LinkIcon } from '../../images/link-icon.svg'
 import { ReactComponent as MetaMaskLogo } from '../../images/metamask-logo.svg'
+import { ReactComponent as WarningIcon } from '../../images/warning.svg'
 import useWatchAsset from '../../hooks/use-watch-asset'
 
 const explorerUrl = {
@@ -27,6 +28,28 @@ function TxOverview ({ wallet, swapData, fromTokenInfo, toTokenInfo, onGoBack, o
       />
       <CheckIcon className={classes.checkIcon} />
       <p className={classes.title}>{fromTokenInfo.symbol} token conversion to {toTokenInfo.symbol} has been completed.</p>
+      {wallet.chainId === '100' && (
+        <div className={classes.note}>
+          <p className={classes.noteTitle}>
+            <WarningIcon width="18px" /> Note
+          </p>
+          <p>
+            If you want to send {toTokenInfo.symbol} tokens to a centralized exchange,
+            be sure to use{' '}
+            <a
+              className={classes.noteLink}
+              href="https://omni.xdaichain.com/"
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Omnibridge
+            </a>
+            {' '}to transfer {toTokenInfo.symbol} to Ethereum chain first.<br/>
+            DO NOT send {toTokenInfo.symbol} tokens from xDai Chain to Binance, Kraken or
+            other centralized exchanges or your tokens will be LOST!
+          </p>
+        </div>
+      )}
       <div className={classes.buttonGroup}>
         <a
           className={classes.howToUseLink}
@@ -34,7 +57,7 @@ function TxOverview ({ wallet, swapData, fromTokenInfo, toTokenInfo, onGoBack, o
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn how to use GNO
+          Learn how to use {toTokenInfo.symbol}
         </a>
         <a
           className={classes.button}
