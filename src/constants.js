9263154c27f148dd0d0c1fd351e378d54742e42e
Ale Banzas <alebanzas@outlook.com>
2022-11-22 14:59:28 +0100

    removing REACT_APP_WRAPPED_TOKEN_CONTRACT_ADDRESS

--- src/constants.js
+++ src/constants.js
@@ -13,7 +13,6 @@ const NETWORKS = {
     addresses: {
       wrapper: '0x647507A70Ff598F386CB96ae5046486389368C66',
       token: '0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb',
-      wrappedToken: '0x722fc4DAABFEaff81b97894fC623f91814a1BF68',
       deposit: '0x0B98057eA310F4d31F2a452B414647007d1645d9',
       dappnodeDeposit: '0x6C68322cf55f5f025F2aebd93a28761182d077c3',
     },
@@ -31,7 +30,6 @@ const NETWORKS = {
     addresses: {
       wrapper: '0x917947dC7E341d843ab38e91623bcAeb65512b75',
       token: '0x19C653Da7c37c66208fbfbE8908A5051B57b4C70',
-      wrappedToken: '0xc5be8bf53755a41c2385e7aa86f6a9e28746f466',
       deposit: '0xb97036A26259B7147018913bD58a774cf91acf25',
       dappnodeDeposit: null,
     },
