211390ac68e1eebf530051f1ab3bba07470b923d
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2024-08-11 18:02:40 +0200

    css for footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="w-full lg:w-[625px] h-full lg:h-auto bg-green backdrop-blur-sm p-8 lg:rounded-2xl flex gap-y-4 flex-col justify-start items-center">
         <div className="w-full lg:w-[550px] h-60 border-b flex gap-y-2 flex-col justify-end items-center pb-2">
           <Image src="/logo.svg" alt="Gnosis Logo" width={100} height={24} />
