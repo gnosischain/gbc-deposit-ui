73859766e2ea0788b14a44f3866e3875de3427cd
Wagalidoom <lukas-8@live.fr>
2024-09-30 12:26:11 +0200

    feat: init new envio indexer

---
+++ envio/src/EventHandlers.ts
@@ -0,0 +1,21 @@
+/*
+ * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
+ */
+import {
+  SBCDepositContract,
+  SBCDepositContract_DepositEvent,
+} from "generated";
+
+SBCDepositContract.DepositEvent.handler(async ({ event, context }) => {
+  const entity: SBCDepositContract_DepositEvent = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    pubkey: event.params.pubkey,
+    withdrawal_credentials: event.params.withdrawal_credentials,
+    amount: event.params.amount,
+    signature: event.params.signature,
+    index: event.params.index,
+  };
+
+  context.SBCDepositContract_DepositEvent.set(entity);
+});
+
--- envio/test/Test.ts
+++ envio/test/Test.ts
@@ -1,7 +1,7 @@
 import assert from "assert";
 import { 
   TestHelpers,
-  SBCDepositContract_DepositEventEntity
+  SBCDepositContract_DepositEvent
 } from "generated";
 const { MockDb, SBCDepositContract } = TestHelpers;
 
@@ -12,21 +12,21 @@ describe("SBCDepositContract contract DepositEvent event tests", () => {
   // Creating mock for SBCDepositContract contract DepositEvent event
   const event = SBCDepositContract.DepositEvent.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
 
-  // Processing the event
-  const mockDbUpdated = SBCDepositContract.DepositEvent.processEvent({
-    event,
-    mockDb,
-  });
+  it("SBCDepositContract_DepositEvent is created correctly", async () => {
+    // Processing the event
+    const mockDbUpdated = await SBCDepositContract.DepositEvent.processEvent({
+      event,
+      mockDb,
+    });
 
-  it("SBCDepositContract_DepositEventEntity is created correctly", () => {
     // Getting the actual entity from the mock database
-    let actualSBCDepositContractDepositEventEntity = mockDbUpdated.entities.SBCDepositContract_DepositEvent.get(
-      `${event.transactionHash}_${event.logIndex}`
+    let actualSBCDepositContractDepositEvent = mockDbUpdated.entities.SBCDepositContract_DepositEvent.get(
+      `${event.chainId}_${event.block.number}_${event.logIndex}`
     );
 
     // Creating the expected entity
-    const expectedSBCDepositContractDepositEventEntity: SBCDepositContract_DepositEventEntity = {
-      id: `${event.transactionHash}_${event.logIndex}`,
+    const expectedSBCDepositContractDepositEvent: SBCDepositContract_DepositEvent = {
+      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
       pubkey: event.params.pubkey,
       withdrawal_credentials: event.params.withdrawal_credentials,
       amount: event.params.amount,
@@ -34,6 +34,6 @@ describe("SBCDepositContract contract DepositEvent event tests", () => {
       index: event.params.index,
     };
     // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(actualSBCDepositContractDepositEventEntity, expectedSBCDepositContractDepositEventEntity, "Actual SBCDepositContractDepositEventEntity should be the same as the expectedSBCDepositContractDepositEventEntity");
+    assert.deepEqual(actualSBCDepositContractDepositEvent, expectedSBCDepositContractDepositEvent, "Actual SBCDepositContractDepositEvent should be the same as the expectedSBCDepositContractDepositEvent");
   });
 });
