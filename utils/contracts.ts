4be125637c7c5aca371dcd5be03a228cc6c92e8a
Wagalidoom <lukas-8@live.fr>
2025-03-10 18:26:27 +0100

    chore: update chiado addresses fo pectra

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -41,6 +41,7 @@ const CONTRACTS: Contracts = {
       token: "0x19C653Da7c37c66208fbfbE8908A5051B57b4C70",
       deposit: "0xb97036A26259B7147018913bD58a774cf91acf25",
       claimRegistry: "0x28f1ba1f2Db9Aa0ca4b3B7cD9Ae327f6E872867D",
+      consolidate: "0x0000BBdDc7CE488642fb579F8B00f3a590007251"
     },
     depositStartBlockNumber: BigInt(155530),
     claimRegistryStartBlockNumber: BigInt(9311142),
--- wagmi.ts
+++ wagmi.ts
@@ -6,13 +6,15 @@ import { devnet } from "./utils/devnet";
 export const isTestEnv = process.env.NEXT_PUBLIC_TEST_ENV === "test";
 
 export const config = createConfig({
-  chains: [gnosis, gnosisChiado, devnet, ...(isTestEnv ? [hardhat] : [])],
+  chains: [gnosis, gnosisChiado,
+    // devnet,
+    ...(isTestEnv ? [hardhat] : [])],
   connectors: [coinbaseWallet({ appName: "Gnosis Deposit" }), walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID || "" })],
   ssr: true,
   transports: {
     [gnosis.id]: http("https://rpc.gnosischain.com/"),
     [gnosisChiado.id]: http("https://rpc.chiadochain.net"),
-    [devnet.id]: http("https://gnosis-devnet.nethermind.dev/execution"),
+    // [devnet.id]: http("https://gnosis-devnet.nethermind.dev/execution"),
     [hardhat.id]: http("http://127.0.0.1:8545"),
   },
 });
